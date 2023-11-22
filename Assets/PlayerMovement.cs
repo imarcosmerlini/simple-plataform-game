e127c96e9198f012cfa19d185dc769129ae78f52
Marcos Merlini <merlinimarcos@gmail.com>
2023-11-22 12:49:17 -0300

    created base script to player with jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(("space")))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+        }
     }
 }
