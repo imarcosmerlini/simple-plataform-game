81b1a5ae21a017f198020262f4cb1827328c2d9a
Marcos Merlini <merlinimarcos@gmail.com>
2023-11-22 22:30:57 -0300

    created horizontal movento for player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(("space")))
+        float dirX = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(dirX * 7f, _rb.velocity.y);
+        
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            _rb.velocity = new Vector3(_rb.velocity.x, 14f);
         }
     }
 }
