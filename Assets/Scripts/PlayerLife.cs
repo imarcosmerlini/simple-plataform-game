c211d8b1d1b9ec95682d6cc478f9a1da74b20886
Marcos Merlini <merlinimarcos@gmail.com>
2023-11-27 21:45:17 -0300

    created basic death interaction

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int Death = Animator.StringToHash("death");
+    private Rigidbody2D _rigidbody;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }   
+    }
+
+    private void Die()
+    {
+        _rigidbody.bodyType = RigidbodyType2D.Static;
+        _animator.SetTrigger(Death);
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
