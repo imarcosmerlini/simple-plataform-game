1e1d5017e962d6703a9cf40310cb8d75b9a1c978
Marcos Merlini <merlinimarcos@gmail.com>
2023-12-01 23:59:26 -0300

    created new scene

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource _finishSound;
+    private bool levelCompleted = false;
+    
+    void Start()
+    {
+        _finishSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !levelCompleted)
+        {
+            _finishSound.Play();
+            levelCompleted = true;
+            Invoke(nameof(CompleteLevel), 2f);
+        }
+    }
+
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
